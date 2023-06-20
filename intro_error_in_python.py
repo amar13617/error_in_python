8880600befc98bfad8bd08aa30eb4221bbd5b134
LENOVO <amar13617@gmail.com>
2023-06-20 13:47:52 +0530

    intro of an error

---
+++ intro_error_in_python.py
@@ -0,0 +1,9 @@
+#print(my_variable) #name error
+
+#when you get an error
+#1. Look at your code!
+#2. Put the error and message into Google, see if something comes up in StackOverflow.
+#3. Look at the code you’ve written again, this time more slowly, and run through it as if you were a computer. Do you notice anything that could potentially be a source of the error?
+#4. Run only some parts of the code in isolation, that’ll help identify which part of your code is giving you an error.
+#5. Use a debugger (we’ll learn how in the next couple videos).
+#6. And of course, ask questions in the Course Q&A. We’re here to help!

62a1d9d77f2c53d6aeea1c3b30a2db4c354548c6
LENOVO <amar13617@gmail.com>
2023-06-20 15:43:19 +0530

    built error is committed

---
+++ built_error.py
@@ -0,0 +1,50 @@
+#1
+#friends = ["rolf","anne"]
+#print(friends[2]) #index error
+#2
+#def show_movie(movie):
+#    print(f"Name: {movie['name']}")
+#    print(f"Name: {movie['director']}")
+#    print(f"Name: {movie['release']}") #it should be year
+    
+#keyError: 'release'
+#3
+#print(hello)
+#NameError: name 'hello' is not defined
+
+#4
+#friends = ['Rolf', 'Jose', 'Charlie']
+#friends_nearby = ['Rolf', 'Anna']
+#friends.intersection(friends_nearby)
+
+#AttributeError: 'list' object has no attributeerror
+
+#5
+#class User
+#    def __init__(self,username, password):
+#        self.username = username
+#        self.password = password
+        
+#SyntaxError: expected ':'
+
+#6
+#def add_two(x,y):
+#return x + y
+
+#IndentationError: expected an indented block 
+
+#7
+#def pow(n,exp):
+#  return n ** exp #tab error
+
+#8
+#value = 6 + 'hi'
+#print(value)
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+#9
+#value = int('20.8')
+#print(value)
+
+
+
